91df21fe954fcd059fa24bc626e080a4573f2801
Santosh V. Madhyastha <santosh@gogii.net>
2017-11-04 16:32:17 -0700

    Init commit of Blank project

---
+++ app/src/main/java/android/santosh/com/filecodechallenge/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package android.santosh.com.filecodechallenge.activity;
+
+import android.santosh.com.filecodechallenge.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
